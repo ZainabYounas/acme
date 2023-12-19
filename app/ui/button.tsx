eb11599d2ea4b3a5ea7a8c3af3a1f242cb2a1705
Zainab <zainabyounas909@gmail.com>
2023-12-19 17:32:33 +0500

    added invoices functionality

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-
+import { deleteInvoice } from '@/app/lib/actions';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
