1fc314f81009391b85b9b7cf20d7c123a35f3e09
Zainab <zainabyounas909@gmail.com>
2023-12-20 17:26:53 +0500

    added authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,9 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { AuthError } from 'next-auth';
+import { signIn } from '@/auth';
+
 export type State = {
     errors?: {
       customerId?: string[];
@@ -11,6 +14,25 @@ export type State = {
     };
     message?: string | null;
   };
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
+  }
 
 const FormSchema = z.object({
     id: z.string(),
@@ -114,3 +136,4 @@ const FormSchema = z.object({
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
   }
+
